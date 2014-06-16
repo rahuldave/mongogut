3c57adba74448990496063871e1ae9823afdf17d
Rahul <rahul@aglianico.cfa.harvard.edu>
2014-06-16 10:09:17 -0400

    inst.sh parametric

--- mongogut/inst.sh
+++ mongogut/inst.sh
@@ -1,4 +1,4 @@
-  965  sh dbit adsgut3
-  966  python social.py adsgut3
-  967  python ptassets.py adsgut3
-  968  time python migrator.py /Users/rahul/play/adsgut_20140612-154709/adsgut  adsgut3
+sh dbit $1
+python social.py $1
+python ptassets.py $1
+time python migrator.py /Users/rahul/play/adsgut_20140612-154709/adsgut $1
