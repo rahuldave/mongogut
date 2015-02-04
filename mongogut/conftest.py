f0bae36833eaaaa803a926a5ab7a3d960d48e0b2
Rahul Dave <rahuldave@gmail.com>
2015-02-03 23:37:26 -0500

    more test machinery

--- mongogut/conftest.py
+++ mongogut/conftest.py
@@ -1,6 +1,8 @@
 # test_fixtures.py
 import pytest
 from pytest import fixture
+#import warnings
+#warnings.simplefilter('error')
 from mongoengine import connect
 from social import _init as socialinit
 from social import initialize_testing as social_testinit
@@ -10,18 +12,22 @@ from ptassets import initialize_testing as ptassets_testinit
 from ptassets import Postdb
 
 @fixture(scope="module")  # Registering this function as a fixture.
-def postdb():
-	db_session=connect('testgut', host='mongodb://adsgut:adsgut@localhost:27017/testgut')
-	db_session.drop_database('testgut')
-	socialinit('testgut', 'mongodb://adsgut:adsgut@localhost:27017/testgut')
-	ptassetsinit('testgut', 'mongodb://adsgut:adsgut@localhost:27017/testgut')
-	social_testinit(db_session)
-	ptassets_testinit(db_session)
-	#db_session=connect('testgut', host='mongodb://adsgut:adsgut@localhost:27017/testgut')
-	postdb=Postdb(db_session)
-	return postdb
+def postdb(request):
+    db_session=connect('testgut', host='mongodb://adsgut:adsgut@localhost:27017/testgut')
+    #db_session.drop_database('testgut')
+    socialinit('testgut', 'mongodb://adsgut:adsgut@localhost:27017/testgut')
+    ptassetsinit('testgut', 'mongodb://adsgut:adsgut@localhost:27017/testgut')
+    social_testinit(db_session)
+    ptassets_testinit(db_session)
+    #db_session=connect('testgut', host='mongodb://adsgut:adsgut@localhost:27017/testgut')
+    postdb=Postdb(db_session)
+    def teardown():
+        db_session.drop_database('testgut')
+    request.addfinalizer(teardown)
+    
+    return postdb
 
 
 if __name__=="__main__":
-	pytest.main()
+    pytest.main()
 
--- mongogut/test_social.py
+++ mongogut/test_social.py
@@ -1,17 +1,45 @@
 import pytest
+from pytest import raises
+from exc import MongoGutError
+
+def notallowed(assertion):
+    with raises(MongoGutError) as excinfo:
+        assert assertion
+    assert 1==1, "["+str(excinfo.value.message)+" "+str(excinfo.value.status_code)+"]"
+
+def print_exc(excinfo):
+    print ">>>"+str(excinfo.value.message)+"<<<"+str(excinfo.value.status_code)+"==="
+class Inifix:
+
+    def __init__(self, postdb):
+        self.whosdb = postdb.whosdb
+        currentuser=None
+        adsgutuser=self.whosdb._getUserForNick(currentuser, "adsgut")
+        adsuser=self.whosdb._getUserForNick(adsgutuser, "ads")
+        rahuldave=self.whosdb._getUserForNick(adsgutuser, "rahuldave")
+        jayluker=self.whosdb._getUserForNick(adsgutuser, "jayluker")
+        self.users = dict(adsgut=adsgutuser,
+        ads=adsuser, rahuldave=rahuldave, jayluker=jayluker)
 
 @pytest.fixture(scope="module")
-def iniuserdb(postdb):
-    whosdb=postdb.whosdb
-    currentuser=None
-    adsgutuser=whosdb._getUserForNick(currentuser, "adsgut")
-    adsuser=whosdb._getUserForNick(adsgutuser, "ads")
-    rahuldave=whosdb._getUserForNick(adsgutuser, "rahuldave")
-    jayluker=whosdb._getUserForNick(adsgutuser, "jayluker")
-    iniuserdb=dict(db=whosdb, users=dict(adsgut=adsgutuser,
-        ads=adsuser, rahuldave=rahuldave, jayluker=jayluker))
-    return iniuserdb
+def inidb(postdb):
+    return Inifix(postdb)
 
+def test_social0(inidb):
+    assert inidb.users['rahuldave'].nick=="rahuldave"
 
-def test_social0(iniuserdb):
-    assert iniuserdb['users']['rahuldave'].nick=="rahuldave"
+#@pytest.mark.usefixtures("iniuserdb")
+#class TestSocialUser:
+def test_users_whotheyare(inidb, capsys):
+    assert inidb.whosdb.isSystemUser(inidb.users['adsgut'])
+    assert inidb.whosdb.getUserInfo(inidb.users['rahuldave'], "rahuldave")
+    assert inidb.whosdb.getUserInfo(inidb.users['adsgut'], "rahuldave")
+    #notallowed(inidb.whosdb.getUserInfo(inidb.users['jayluker'], "rahuldave"))
+    out, err = capsys.readouterr()
+    with raises(MongoGutError) as excinfo:
+        assert inidb.whosdb.getUserInfo(inidb.users['jayluker'], "rahuldave")
+    print_exc(excinfo)
+    out, err = capsys.readouterr()
+    assert "author" in out
+    print out
+    #assert "uthor" in excinfo.message
